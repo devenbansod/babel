e8c742c33fb3c6d99871bcb9f5ee4a2408dde5ea
Deven Bansod <devenbansod.bits@gmail.com>
2018-03-23 21:02:53 +0530

    8

--- packages/babel-preset-es2015/test/fixtures/traceur/TemplateLiterals/Tag.js
+++ packages/babel-preset-es2015/test/fixtures/traceur/TemplateLiterals/Tag.js
@@ -39,53 +39,49 @@
   expect(expose``[0]).toHaveLength(1);
   expect(expose``[0].raw).toHaveLength(1);
 
-  expect(expose`a`[0].raw).toEqual(['a']);;
-  expect(expose`a`[0]).toEqual(['a']);;
+  expect(expose`a`[0].raw).toEqual(['a']);
+  expect(expose`a`[0]).toEqual(['a']);
 
-  expect(expose`\n`[0].raw).toEqual(['\\n']);;
-  expect(expose`\n`[0]).toEqual(['\n']);;
+  expect(expose`\n`[0].raw).toEqual(['\\n']);
+  expect(expose`\n`[0]).toEqual(['\n']);
 
-  expect(expose`\r`[0].raw).toEqual(['\\r']);;
-  expect(expose`\r`[0]).toEqual(['\r']);;
+  expect(expose`\r`[0].raw).toEqual(['\\r']);
+  expect(expose`\r`[0]).toEqual(['\r']);
 
-  expect(expose`\f`[0].raw).toEqual(['\\f']);;
-  expect(expose`\f`[0]).toEqual(['\f']);;
+  expect(expose`\f`[0].raw).toEqual(['\\f']);
+  expect(expose`\f`[0]).toEqual(['\f']);
 
-  expect(expose`\b`[0].raw).toEqual(['\\b']);;
-  expect(expose`\b`[0]).toEqual(['\b']);;
+  expect(expose`\b`[0].raw).toEqual(['\\b']);
+  expect(expose`\b`[0]).toEqual(['\b']);
 
-  expect(expose`\u2028`[0].raw).toEqual(['\\u2028']);;
-  expect(expose`\u2028`[0]).toEqual(['\u2028']);;
+  expect(expose`\u2028`[0].raw).toEqual(['\\u2028']);
+  expect(expose`\u2028`[0]).toEqual(['\u2028']);
 
-  expect(expose`\u2029`[0].raw).toEqual(['\\u2029']);;
-  expect(expose`\u2029`[0]).toEqual(['\u2029']);;
+  expect(expose`\u2029`[0].raw).toEqual(['\\u2029']);
+  expect(expose`\u2029`[0]).toEqual(['\u2029']);
 
-  expect(expose`a${x}b`[0].raw).toEqual(['a', 'b']);;
-  expect(expose`a${x}b`[0]).toEqual(['a', 'b']);;
+  expect(expose`a${x}b`[0].raw).toEqual(['a', 'b']);
+  expect(expose`a${x}b`[0]).toEqual(['a', 'b']);
 
   // These have tab characters in them.
-  expect(expose`	${x}\t`[0].raw).toEqual(['\t', '\\t']);;
-  expect(expose`	${x}\t`[0]).toEqual(['\t', '\t']);;
+  expect(expose`	${x}\t`[0].raw).toEqual(['\t', '\\t']);
+  expect(expose`	${x}\t`[0]).toEqual(['\t', '\t']);
 
   expect(expose`
-${x}\n`[0].raw).toEqual(['\n', '\\n']);;
+${x}\n`[0].raw).toEqual(['\n', '\\n']);
   expect(expose`
-${x}\n`[0]).toEqual(['\n', '\n']);;
+${x}\n`[0]).toEqual(['\n', '\n']);
 
   // These contains the ES new line chars \u2028 and \u2029
-  expect(expose`
-${x}\u2028`[0].raw).toEqual(['\u2028', '\\u2028']);;
-  expect(expose`
-${x}\u2028`[0]).toEqual(['\u2028', '\u2028']);;
+  expect(expose` ${x}\u2028`[0].raw).toEqual(['\u2028', '\\u2028']);
+  expect(expose` ${x}\u2028`[0]).toEqual(['\u2028', '\u2028']);
 
-  expect(expose`
-${x}\u2029`[0].raw).toEqual(['\u2029', '\\u2029']);;
-  expect(expose`
-${x}\u2029`[0]).toEqual(['\u2029', '\u2029']);;
+  expect(expose` ${x}\u2029`[0].raw).toEqual(['\u2029', '\\u2029']);
+  expect(expose` ${x}\u2029`[0]).toEqual(['\u2029', '\u2029']);
 
-  expect(expose`a/*b*/c`[0].raw).toEqual(['a/*b*/c']);;
-  expect(expose`a/*b*/c`[0]).toEqual(['a/*b*/c']);;
+  expect(expose`a/*b*/c`[0].raw).toEqual(['a/*b*/c']);
+  expect(expose`a/*b*/c`[0]).toEqual(['a/*b*/c']);
 
-  expect(expose/* comment */`a`[0].raw).toEqual(['a']);;
-  expect(expose/* comment */`a`[0]).toEqual(['a']);;
+  expect(expose/* comment */`a`[0].raw).toEqual(['a']);
+  expect(expose/* comment */`a`[0]).toEqual(['a']);
 };
